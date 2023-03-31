9c7e420d078d931e11496f658c1b73207756c5ef
Derwin <verdecwc@gmail.com>
2023-03-31 10:02:41 -0700

    Updated

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 850, function(){
+      }, 800, function(){
    
         window.location.hash = hash;
       });
@@ -20,40 +20,27 @@ $(document).ready(function(){
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '.navbar',
-    offset: 100
+    offset: 50
   })
 
   // Collapse navbar on click
   $(".navbar-collapse a").click(function() {
     $(".navbar-collapse").collapse('hide');
   });
-});
 
-$(document).ready(function() {
-    // Smooth scrolling on click
-    $('a[href^="#"]').on('click', function(event) {
-      var target = $(this.getAttribute('href'));
-      if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-          scrollTop: target.offset().top - 100
-        }, 1000);
-      }
-    });
+  // Filter projects
+  $('.filter-btn').on('click', function() {
+    var category = $(this).attr('data-category');
     
-    // Filter projects
-    $('.filter-btn').on('click', function() {
-      var category = $(this).attr('data-category');
-      
-      $('.project').fadeOut(500);
-      
-      setTimeout(function() {
-        if(category == 'all') {
-          $('.project').fadeIn(500);
-        } else {
-          $('.' + category).fadeIn(500);
-        }
-      }, 500);
-    });
+    $('.project').fadeOut(500);
+    
+    setTimeout(function() {
+      if(category == 'all') {
+        $('.project').fadeIn(500);
+      } else {
+        $('.' + category).fadeIn(500);
+      }
+    }, 500);
   });
+});
   
